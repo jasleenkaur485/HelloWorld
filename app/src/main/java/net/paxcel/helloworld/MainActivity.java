5d33a7587a408b330895fcd0268bade1856dd077
jasleenkaur485 <jasleen.kaur@paxcel.net>
2016-08-10 14:50:22 +0530

    testing commit newBranch1

--- app/src/main/java/net/paxcel/helloworld/MainActivity.java
+++ app/src/main/java/net/paxcel/helloworld/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("test","tesbasd");
+        Log.i("test","newbranch1Code");
     }
 }
